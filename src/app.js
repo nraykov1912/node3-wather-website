b89a80f47f4293d21a1140b17b2e4e56738ac89e
Nikolay Raykov <nraykov@Nikolays-MacBook-Air.local>
2021-11-02 02:00:15 +0200

    Init commit

---
+++ src/app.js
@@ -0,0 +1,102 @@
+const path = require('path')
+const express = require('express')
+const hbs = require('hbs')
+const geocode = require('./utils/geocode')
+const forecast = require('./utils/forecast')
+
+const app = express()
+
+//Define paths for Express config
+const publicDirectory = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
+
+//Setup handlesbars engine and views location
+app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
+
+//Setup static directory to serve
+app.use(express.static(publicDirectory))
+
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather',
+        name: 'Nikolay Raykov'
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About me',
+        name: 'Nikolay Raykov'
+    })
+})
+
+app.get('/products', (req,res) => {
+    if (!req.query.search) {
+        return res.send({
+            error: 'You must provide a search term'
+        })
+    }
+
+    console.log(req.query.search)
+    res.send({
+        products: []
+    })
+})
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        helpText: 'Helpful text',
+        title: 'Help',
+        name: 'Nikolay Raykov'
+    })
+})
+
+app.get('/weather', (req, res) => {
+    const address = req.query.address
+
+    if (!address) {
+        return res.send({
+            error: 'You must provide an address.'
+        })
+    }
+
+    geocode(address, (error, {latitude, longitude, location} = {}) =>{
+        if(error){
+            return res.send({error})
+        }
+        forecast(latitude, longitude, (error, forecastData) => {
+            if(error){
+                return res.send({error})
+            }
+            res.send({
+                forecast: forecastData,
+                location,
+                address: address
+            })
+        })
+    })
+
+})
+
+app.get('/help/*', (req,res) => {
+    res.render('404',{
+        title: '404',
+        name: 'Nikolay Raykov',
+        errorMessage: 'Help article not found'
+    })
+})
+
+app.get('*', (req, res) => {
+    res.render('404',{
+        title: '404',
+        name: 'Nikolay Raykov',
+        errorMessage: 'Page not found'
+    })
+})
+
+app.listen(3000, () => {
+    console.log('Server is up on port 3000.')
+})
---
+++ src/utils/forecast.js
@@ -0,0 +1,21 @@
+const request = require('request')
+
+const forecast = (latitude, longitude, callback) => {
+    const url = 'http://api.weatherstack.com/current?access_key=2f209d2c89cfa195c1fe59828acd26ed&query= '+latitude + ',' + longitude + '&units=m'
+
+    request({url, json:true}, (error, {body}) => {
+        if(error){
+            callback('Unable to connect to weather service!', undefined)
+        } else if(body.error){
+            callback('Unable to find location.', undefined)
+        } else {
+            const temperature = body.current.temperature
+            const feelslike = body.current.feelslike
+            const weatherDescription = body.current.weather_descriptions[0]
+            const foreCastData = weatherDescription + '. It is currenlty ' + temperature + ' degrees. It feels like ' + feelslike + ' degrees'
+            callback(undefined, foreCastData)
+        }
+    })
+}
+
+module.exports = forecast
